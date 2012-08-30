68bb4056f65ee8d3ad6d155c23ab03af36054b40
Rafael Maia de Souza <rafael.maia09@gmail.com>
2012-08-30 20:38:51 -0300

    Commenting method

--- Achei.Amigos.Web/Controllers/HomeController.cs
+++ Achei.Amigos.Web/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace Achei.Amigos.Web.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
