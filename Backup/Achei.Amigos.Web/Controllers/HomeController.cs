ad1bda694d8efd9ddbcb591630e14707b30062e9
paulo-ortins <paulo.ortins@gmail.com>
2012-08-30 21:31:08 -0300

    e

---
+++ Backup/Achei.Amigos.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Achei.Amigos.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Backup/Achei.Amigos.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Achei.Amigos.Web.WebApiApplication" Language="C#" %>
